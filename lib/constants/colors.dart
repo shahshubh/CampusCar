2c1121ac2514ae0d68ba71ffef200196be0fc9bc
parshvass <parshvass@somaiya.edu>
2021-02-12 11:35:11 +0530

    Update colors.dart

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 const primaryBlue = Color(0xff4850df);
 const secondaryBlue = Color(0xff131c4a);
+const blue3 = Color(0xff12143e);
+const blue4 = Color(0xff2C6DCA);
+const blue5 = Color(0xff757AFE);
+const blue6 = Color(0xff87ACDF);
