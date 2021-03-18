ed34115ce0ae926964059db55b7f6e4bffad6b7a
shubh <shahshubh1010@gmail.com>
2021-03-18 16:32:14 +0530

    added color and image picker for new vehicle form

--- lib/widgets/new_vehicle_form.dart
+++ lib/widgets/new_vehicle_form.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
 import 'dart:math';
 
 import 'package:CampusCar/widgets/custom_input_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 
-class NewVehicleForm extends StatelessWidget {
+class NewVehicleForm extends StatefulWidget {
   final bool isAdmin;
   final TextEditingController nameTextController;
   final TextEditingController mobileTextController;
@@ -13,14 +15,15 @@ class NewVehicleForm extends StatelessWidget {
   final TextEditingController modelTextController;
   final TextEditingController roleTextController;
   final String role;
-  final Map<String, String> errorText;
+  final Color color;
+  final pickedImage;
   final bool profileImageCheckbox;
   final DateTime expiryDate;
   final Function setExpiryDate;
   final Function setRole;
-  final Function setErrorText;
   final Function setProfileImageCheckbox;
   final Function setPickedImage;
+  final Function setColor;
   NewVehicleForm({
     @required this.isAdmin,
     @required this.nameTextController,
@@ -29,37 +32,89 @@ class NewVehicleForm extends StatelessWidget {
     @required this.modelTextController,
     @required this.roleTextController,
     @required this.role,
-    @required this.errorText,
+    @required this.color,
+    @required this.pickedImage,
     @required this.profileImageCheckbox,
     @required this.expiryDate,
     @required this.setExpiryDate,
-    @required this.setErrorText,
     @required this.setProfileImageCheckbox,
     @required this.setRole,
     @required this.setPickedImage,
+    @required this.setColor,
   });
 
+  @override
+  _NewVehicleFormState createState() => _NewVehicleFormState();
+}
+
+class _NewVehicleFormState extends State<NewVehicleForm> {
   final picker = ImagePicker();
+  Color tempColor = Colors.red;
+
+  void _openDialog(String title, Widget content, context) {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.all(6.0),
+          title: Text(title),
+          content: content,
+          actions: [
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: Navigator.of(context).pop,
+            ),
+            FlatButton(
+              child: Text('SUBMIT'),
+              onPressed: () {
+                widget.setColor(tempColor);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _openColorPicker(context) async {
+    _openDialog(
+        "Color picker",
+        MaterialColorPicker(
+          // allowShades: false,
+          selectedColor: widget.color,
+          onColorChange: (color) {
+            print(color);
+            setState(() {
+              tempColor = color;
+            });
+          },
+          // onMainColorChange: (color) => print(color),
+          colors: [Colors.red, Colors.green],
+          // onBack: () => print("Back button pressed"),
+        ),
+        context);
+  }
 
   void getImage({String source}) async {
     ImageSource imageSource = ImageSource.camera;
     var pickedFile = await picker.getImage(source: imageSource);
     if (pickedFile != null) {
-      setPickedImage(pickedFile);
+      widget.setPickedImage(pickedFile);
     }
   }
 
   void selectDate(context) async {
-    if (isAdmin) {
+    if (widget.isAdmin) {
       final DateTime newDate = await showDatePicker(
         context: context,
-        initialDate: expiryDate,
+        initialDate: widget.expiryDate,
         firstDate: DateTime(2019, 1),
         lastDate: DateTime(2030, 7),
         helpText: 'Select a date',
       );
       if (newDate != null) {
-        setExpiryDate(newDate);
+        widget.setExpiryDate(newDate);
       }
     }
   }
@@ -69,24 +124,21 @@ class NewVehicleForm extends StatelessWidget {
     return Column(
       children: [
         CustomInputField(
-          controller: nameTextController,
+          controller: widget.nameTextController,
           labelText: "Name",
-          errorText: errorText["name"] != "" ? errorText["name"] : null,
         ),
         SizedBox(height: 20),
 
         CustomInputField(
-          controller: mobileTextController,
+          controller: widget.mobileTextController,
           labelText: "Mobile No.",
-          errorText: errorText["mobile"] != "" ? errorText["mobile"] : null,
           keyboardType: TextInputType.number,
         ),
         SizedBox(height: 20),
 
         CustomInputField(
-          controller: licenseTextController,
+          controller: widget.licenseTextController,
           labelText: "License Plate No.",
-          errorText: errorText["name"] != "" ? errorText["name"] : null,
         ),
         SizedBox(height: 20),
 
@@ -96,7 +148,7 @@ class NewVehicleForm extends StatelessWidget {
           children: [
             Text("Role"),
             DropdownButton(
-              value: role,
+              value: widget.role,
               items: <String>[
                 'Visitor',
                 'Faculty',
@@ -110,24 +162,22 @@ class NewVehicleForm extends StatelessWidget {
                 );
               }).toList(),
               onChanged: (newValue) {
-                setRole(newValue);
+                widget.setRole(newValue);
               },
             ),
           ],
         ),
-        role == 'Other'
+        widget.role == 'Other'
             ? CustomInputField(
-                controller: roleTextController,
+                controller: widget.roleTextController,
                 labelText: "Role",
-                errorText: errorText["name"] != "" ? errorText["name"] : null,
               )
             : Container(),
         SizedBox(height: 20),
 
         CustomInputField(
-          controller: modelTextController,
+          controller: widget.modelTextController,
           labelText: "Model",
-          errorText: errorText["name"] != "" ? errorText["name"] : null,
         ),
         SizedBox(height: 20),
 
@@ -136,6 +186,13 @@ class NewVehicleForm extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text("Color"),
+            CircleColor(
+              circleSize: 25.0,
+              color: widget.color,
+              onColorChoose: () {
+                _openColorPicker(context);
+              },
+            ),
           ],
         ),
 
@@ -149,14 +206,14 @@ class NewVehicleForm extends StatelessWidget {
                 children: [
                   Text(
                     DateFormat("dd, MMMM yyyy hh:mm aa")
-                        .format(DateTime.parse(expiryDate.toString())),
-                    style:
-                        TextStyle(color: isAdmin ? Colors.black : Colors.grey),
+                        .format(DateTime.parse(widget.expiryDate.toString())),
+                    style: TextStyle(
+                        color: widget.isAdmin ? Colors.black : Colors.grey),
                   ),
                   IconButton(
                     icon: Icon(
                       Icons.calendar_today,
-                      color: isAdmin ? Colors.black : Colors.grey,
+                      color: widget.isAdmin ? Colors.black : Colors.grey,
                     ),
                     onPressed: () {
                       selectDate(context);
@@ -175,13 +232,13 @@ class NewVehicleForm extends StatelessWidget {
             Text("Auto select default profile image"),
             Checkbox(
               onChanged: (value) {
-                setProfileImageCheckbox(value);
+                widget.setProfileImageCheckbox(value);
               },
-              value: profileImageCheckbox,
+              value: widget.profileImageCheckbox,
             ),
           ],
         ),
-        profileImageCheckbox != true
+        widget.profileImageCheckbox != true
             ? Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -193,6 +250,15 @@ class NewVehicleForm extends StatelessWidget {
                     },
                   ),
                 ],
+              )
+            : Container(),
+
+        widget.profileImageCheckbox != true && widget.pickedImage != null
+            ? Container(
+                child: Image.file(
+                  File(widget.pickedImage.path),
+                  height: 100,
+                ),
               )
             : Container(),
       ],
