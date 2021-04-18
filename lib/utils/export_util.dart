f169f454172f5ad3779ff4dcb1da611822326f0a
shubh <shahshubh1010@gmail.com>
2021-04-18 18:48:55 +0530

    download as pdf/csv complete

---
+++ lib/utils/export_util.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:csv/csv.dart';
+import 'package:downloads_path_provider/downloads_path_provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pdf/pdf.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+class ExportUtil {
+  static Future<String> get _localPath async {
+    Directory downloadsDirectory =
+        await DownloadsPathProvider.downloadsDirectory;
+    return downloadsDirectory.absolute.path;
+  }
+
+  static Future<File> createLocalFile(
+      {String filename, String fileExtension}) async {
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+    final path = await _localPath;
+    return File('$path/$filename.$fileExtension').create();
+  }
+
+  static saveAsCsv({List<List<dynamic>> rows, String filename}) async {
+    File f = await createLocalFile(filename: filename, fileExtension: "csv");
+    String csv = const ListToCsvConverter().convert(rows);
+    f.writeAsString(csv);
+    Fluttertoast.showToast(msg: "Downloaded Successfully.");
+  }
+
+  static saveAsPdf(
+      {String filename, List headers, List data, String pdfTitle}) async {
+    File file = await createLocalFile(filename: filename, fileExtension: "pdf");
+
+    final pdf = pw.Document();
+    // write on pdf
+    pdf.addPage(
+      pw.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        margin: pw.EdgeInsets.all(32),
+        build: (pw.Context context) {
+          return <pw.Widget>[
+            pw.Header(level: 0, child: pw.Text(pdfTitle)),
+            pw.Table.fromTextArray(headers: headers, data: data)
+          ];
+        },
+      ),
+    );
+
+    // save pdf
+    file.writeAsBytesSync(pdf.save());
+    Fluttertoast.showToast(msg: "Downloaded Successfully.");
+  }
+}
